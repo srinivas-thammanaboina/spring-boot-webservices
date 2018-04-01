0939d8d304edf49e285edc4c6dc820f05fc14d9f
Srinivas <Srinivasthammana99@gmail.com>
2018-04-01 15:18:42 -0700

    updated comments

--- simple-rest-services/src/main/java/com/myspringboot/basics/simplerestservices/SimpleRestServicesApplication.java
+++ simple-rest-services/src/main/java/com/myspringboot/basics/simplerestservices/SimpleRestServicesApplication.java
@@ -2,11 +2,17 @@ package com.myspringboot.basics.simplerestservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SimpleRestServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleRestServicesApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SimpleRestServicesApplication.class, args);
+		
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+		
 	}
 }
